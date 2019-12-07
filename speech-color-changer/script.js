2a111d0459926c8596df9c9a2d2dfe0046b338d8
Anas Bellouzi <anas.bellouzi@students.makeschool.com>
2019-12-07 00:58:42 -0800

    deleted colors

--- speech-color-changer/script.js
+++ speech-color-changer/script.js
@@ -3,6 +3,7 @@ var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
 var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
 
 var colors = [ 'aqua' , 'azure' , 'beige', 'bisque', 'black', 'blue', 'brown', 'chocolate', 'coral', 'crimson', 'cyan', 'fuchsia', 'ghostwhite', 'gold', 'goldenrod', 'gray', 'green', 'indigo', 'ivory', 'khaki', 'lavender', 'lime', 'linen', 'magenta', 'maroon', 'moccasin', 'navy', 'olive', 'orange', 'orchid', 'peru', 'pink', 'plum', 'purple', 'red', 'salmon', 'sienna', 'silver', 'snow', 'tan', 'teal', 'thistle', 'tomato', 'turquoise', 'violet', 'white', 'yellow'];
+
 var grammar = '#JSGF V1.0; grammar colors; public <color> = ' + colors.join(' | ') + ' ;'
 
 var recognition = new SpeechRecognition();
@@ -18,17 +19,20 @@ var diagnostic = document.querySelector('.output');
 var bg = document.querySelector('html');
 var hints = document.querySelector('.hints');
 
-var colorHTML= '';
-colors.forEach(function(v, i, a){
-  console.log(v, i);
-  colorHTML += '<span style="background-color:' + v + ';"> ' + v + ' </span>';
-});
-hints.innerHTML = 'Tap/click then say a color to change the background color of the app. Try '+ colorHTML + '.';
+// var colorHTML= '';
+// colors.forEach(function(v, i, a){
+//   console.log(v, i);
+//   colorHTML += '<span style="background-color:' + v + ';"> ' + v + ' </span>';
+// });
+// hints.innerHTML = 'Tap/click then say a color to change the background color of the app. Try '+ colorHTML + '.';
 
-document.body.onclick = function() {
-  recognition.start();
-  console.log('Ready to receive a color command.');
-}
+
+recognition.start();
+
+// document.body.onclick = function() {
+//
+//   console.log('Ready to receive a color command.');
+// }
 
 recognition.onresult = function(event) {
   // The SpeechRecognitionEvent results property returns a SpeechRecognitionResultList object
@@ -44,9 +48,12 @@ recognition.onresult = function(event) {
   var color = event.results[last][0].transcript;
 
   diagnostic.textContent = 'Result received: ' + color + '.';
-  alert(color)
+
+  if (color == "add")
+
   bg.style.backgroundColor = color;
   console.log('Confidence: ' + event.results[0][0].confidence);
+
 }
 
 recognition.onspeechend = function() {
