68df3ea7391901c0fa5e00acc968ad49ddecffeb
Anas Bellouzi <anas.bellouzi@students.makeschool.com>
2019-12-07 22:01:50 -0800

    completed builder, voice messages

--- script.js
+++ script.js
@@ -1,36 +1,60 @@
+var voiceMessage = document.getElementById('voice_message')
 
-  var editor = grapesjs.init({
-    height: '100%',
-    showOffsets: 1,
-    noticeOnUnload: 0,
-    storageManager: { autoload: 0 },
-    container: '#gjs',
-    fromElement: true,
+var editor = grapesjs.init({
+  height: '100%',
+  showOffsets: 1,
+  noticeOnUnload: 0,
+  storageManager: { autoload: 0 },
+  container: '#gjs',
+  fromElement: true,
 
-    plugins: ['gjs-preset-webpage'],
-    pluginsOpts: {
-      'gjs-preset-webpage': {}
-    }
-  });
-
-
-  window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
-  const synth = window.speechSynthesis;
-  const recognition = new SpeechRecognition();
-
-  const icon = document.querySelector('i.fa.fa-microphone')
-  let paragraph = document.createElement('p');
-  let container = document.querySelector('.text-box');
-  container.appendChild(paragraph);
-  const sound = document.querySelector('.sound');
-
-  icon.addEventListener('click', () => {
-    dictate();
-  });
-
-  const dictate = () => {
-    recognition.start();
-    recognition.onresult = (event) => {
-      const speechToText = event.results[0][0].transcript;
-    }
+  plugins: ['gjs-preset-webpage'],
+  pluginsOpts: {
+    'gjs-preset-webpage': {}
   }
+});
+
+
+window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
+const synth = window.speechSynthesis;
+const recognition = new SpeechRecognition();
+
+const icon = document.querySelector('i.fa.fa-microphone')
+let paragraph = document.createElement('p');
+let container = document.querySelector('.text-box');
+container.appendChild(paragraph);
+const sound = document.querySelector('.sound');
+
+icon.addEventListener('click', () => {
+  dictate();
+});
+
+const dictate = () => {
+  recognition.start();
+  recognition.onresult = (event) => {
+    const speechToText = event.results[0][0].transcript;
+
+    voiceMessage.innerHTML = speechToText
+
+    if (speechToText.includes('create')) {
+      if (speechToText.includes('box')) {
+        createBox(speechToText)
+      }
+    }
+
+  }
+  recognition.onstart = function() {
+    voiceMessage.innerHTML = 'Voice recognition activated. Try speaking into the microphone.'
+  }
+  recognition.onspeechend = function() {
+    voiceMessage.innerHTML = 'You were quiet for a while so voice recognition turned itself off.'
+  }
+}
+
+function createBox(text) {
+    // Append components directly to the canvas
+  editor.addComponents(`<div>
+    <img src="https://path/image" />
+    <span title="foo">${text}</span>
+  </div>`);
+}
