e906083a47d53680db1fa5b8d2a8bbb2e7d77e5e
vladyslavnUA <vladyslavn@yahoo.com>
2019-12-11 13:18:50 -0800

    a

--- script.js
+++ script.js
@@ -87,6 +87,7 @@ var editor = grapesjs.init({
           'https://code.jquery.com/jquery-3.3.1.slim.min.js',
           'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js',
           'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js'
+          // google fonts
       ],
   }
 });
